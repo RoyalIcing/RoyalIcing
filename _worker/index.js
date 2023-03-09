e298089380c6e971e3ce37d273cf786f67a9a218
Patrick Smith <pgwsmith@gmail.com>
2023-03-09 15:22:15 +1100

    Use modern-normalize

--- _worker/index.js
+++ _worker/index.js
@@ -2,6 +2,13 @@ import { serveRequest } from "@collected/press-server";
 
 export default {
   async fetch(request) {
-    return await serveRequest('RoyalIcing', 'RoyalIcing', new URL(request.url));
+    const url = new URL(request.url);
+
+    // TODO: put this into collected press itself?
+    if (url.pathname.startsWith('unpkg.com/modern-normalize@')) {
+      return fetch(`https://${url.pathname}`);
+    }
+
+    return await serveRequest('RoyalIcing', 'RoyalIcing', url);
   }
 }
