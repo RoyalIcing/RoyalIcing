80764e138acb9b39c193cc7c20fa6678f53f267a
Patrick Smith <pgwsmith@gmail.com>
2023-03-16 11:55:49 +1100

    Try and fail to get Deno working

--- _deno/index.ts
+++ _deno/index.ts
@@ -1,15 +1,28 @@
 import { serve } from "https://deno.land/std@0.159.0/http/server.ts";
-import { serveRequest } from "press";
+import { serveRequest } from "npm:@collected/press-server";
 
-const port = 8080;
+const port = 4328;
 
-const handler = (request: Request): Response => {
-  const body = `Your user-agent is:\n\n${
-    request.headers.get("user-agent") ?? "Unknown"
-  }`;
+const handler = async (request: Request): Promise<Response> => {
+  const url = new URL(request.url);
 
-  return new Response(body, { status: 200 });
+  // TODO: put this into collected press itself?
+  if (url.pathname.startsWith("/unpkg.com/modern-normalize@")) {
+    return fetch(`https:/${url.pathname}`);
+  }
+
+  // if (url.pathname.startsWith('/2022/')) {
+  //   url.pathname = url.pathname.replace(/^\/2022\//, "/blog/")
+  //   return new Response(null, {
+  //     status: 307,
+  //     headers: {
+  //       location: url.pathname // Not sure of host with CF Workers, so not putting the full URL.
+  //     }
+  //   })
+  // }
+
+  return await serveRequest("RoyalIcing", "RoyalIcing", url);
 };
 
-console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
+console.log(`HTTP webserver running. Access it at: http://localhost:${port}/`);
 await serve(handler, { port });
