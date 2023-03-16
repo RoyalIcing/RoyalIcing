cd24f5751ce615bab141f896dd8f95e3b3dbbe51
Patrick Smith <pgwsmith@gmail.com>
2023-03-16 15:49:17 +1100

    Allow highlight.js to be loaded from unpkg.com

--- _worker/index.js
+++ _worker/index.js
@@ -1,23 +1,15 @@
 import { serveRequest } from "@collected/press-server";
 
+const allowedUnpkgPackages = ['modern-normalize', 'highlight.js'];
+
 export default {
   async fetch(request) {
     const url = new URL(request.url);
 
     // TODO: put this into collected press itself?
-    if (url.pathname.startsWith('/unpkg.com/modern-normalize@')) {
+    if (allowedUnpkgPackages.some(name => url.pathname.startsWith(`/unpkg.com/${name}@`))) {
       return fetch(`https:/${url.pathname}`);
     }
-
-    // if (url.pathname.startsWith('/2022/')) {
-    //   url.pathname = url.pathname.replace(/^\/2022\//, "/blog/")
-    //   return new Response(null, {
-    //     status: 307,
-    //     headers: {
-    //       location: url.pathname // Not sure of host with CF Workers, so not putting the full URL.
-    //     }
-    //   })
-    // }
 
     return await serveRequest('RoyalIcing', 'RoyalIcing', url);
   }
